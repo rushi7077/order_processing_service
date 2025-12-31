5d27381cf589c9790b26201b4e2df2d5acdd26c8
Rushi Baravkar <rushibaravkar1012@gmail.com>
2025-12-31 20:13:25 +0530

    initial commit

---
+++ src/test/java/com/example/Order_processing_service/OrderProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Order_processing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
